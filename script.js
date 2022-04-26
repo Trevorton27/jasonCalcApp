e0d1374c16b85bffe58ce48e3ce42f229fa83d24
Jason Breedlove <j@jasonbreedlove.com>
2022-04-26 12:05:52 -0700

    buttons

---
+++ script.js
@@ -0,0 +1,13 @@
+ // while user input != 'equals sign' continue operations
+ // if user input == 'equals sign' break operations
+ // if user input == 'clear' clear all
+ // if user input == '+' add
+ // if user input == '-' subtract
+ // if user input == '*' multiply
+ // if user input == '/' divide
+ // if user input == '%' modulus
+ // if user input == '^' exponent
+
+ //function for each operation
+ // call function when user choose an operation to perform
+
