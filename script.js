1029d19b6cdfa7db1f6f80235ca055fe345a6252
Jason Breedlove <j@jasonbreedlove.com>
2022-04-27 16:05:09 -0700

    working with button loops

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
- // while user input != 'equals sign' continue operations
- // if user input == 'equals sign' break operations
- // if user input == 'clear' clear all
- // if user input == '+' add
- // if user input == '-' subtract
- // if user input == '*' multiply
- // if user input == '/' divide
- // if user input == '%' modulus
- // if user input == '^' exponent
+// while user input != 'equals sign' continue operations
+// if user input == 'equals sign' break operations
+// if user input == 'clear' clear all
+//function for each operation
 
- //function for each operation
- // call function when user choose an operation to perform
+// on operator click perform calculations and empty the array then add that to the running total.
+let output = [];
+let oneArray = []
+let sum = 0;
+
+// function for each button
+let input = document.getElementById('input');
+const zero = document.getElementById('zero');
+zero.addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log("button clicked", output)
+});
+const one = document.getElementById('one');
+one.addEventListener('click', function (event,) {
+    event.preventDefault();
+    output.push(1)
+    let myValue = 1;
+        myValue += "1";
+        output.push(myValue);
+        console.log(myValue)
+    // output.push(myValue);
+    addNumbers( output );
+    // document.getElementById('input').innerHTML = output.join('');
+    console.log("one ", output)
+});
+const two = document.getElementById('two');
+two.addEventListener('click', function (event) {
+    event.preventDefault();
+    output.push(2)
+    addNumbers( output );
+    console.log("2 clicked", output)
+});
+
+
+// operators
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function (event) {
+    event.preventDefault();
+    output = [];
+    console.log("clear clicked", output)
+});
+const equals = document.getElementById('equals');
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const decimal = document.getElementById('decimal');
+
+function addNumbers(numbers) {
+    let addButton = document.getElementById('add');
+    addButton.addEventListener('click', function(){
+        for (let i = 0; i < numbers.length; i++) {
+
+            sum += numbers[i];
+            numbers.splice(0, numbers.length);
+        }
+            console.log(sum);
+    });
+
+}
+
+
+
+
 
