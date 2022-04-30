6f82bc60850906aae5049b99343a1db84afd3ed2
Jason Breedlove <j@jasonbreedlove.com>
2022-04-30 15:39:00 -0700

    operator functions

--- calcApp.js
+++ calcApp.js
@@ -13,17 +13,52 @@ let result = null;
 let display1Num = '';
 let display2Num = '';
 let hasDecimal = false;
+let previousOperator = '';
 
-numberElement.forEach(number =>{
-    number.addEventListener('click', (e)=>{
-    if(e.target.innerText === '.' && !hasDecimal){
-        hasDecimal = true;
-    }else if(e.target.innerText === '.' && hasDecimal){
-        return;
-    }
+numberElement.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (e.target.innerText === '.' && !hasDecimal) {
+            hasDecimal = true;
+        } else if (e.target.innerText === '.' && hasDecimal) {
+            return;
+        }
         display2Num += e.target.innerText;
         displayElement2.innerText = display2Num;
 
     });
 
 });
+
+operatorElement.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        displayElement1.innerText = display2Num;
+    });
+});
+
+
+
+function mathOperation(display1Num, display2Num, operator) {
+    if (operatorElement.innerText === '+') {
+        result += parseFloat(display1Num) + parseFloat(display2Num);
+        console.log('working');
+    }
+    if (operatorElement.innerText === '-') {
+        result += parseFloat(display2Num);
+    }
+    if (operatorElement.innerText === '*') {
+        result += parseFloat(display2Num);
+    }
+    if (operatorElement.innerText === '/') {
+        result += parseFloat(display2Num);
+    }
+    if (operatorElement.innerText === '%') {
+        result += parseFloat(display2Num);
+    }
+
+}
+
+// transfer display2Num to display1Num
+
+//update temoResult
+
+//
