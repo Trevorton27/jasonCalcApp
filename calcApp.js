462e613f76cb0f5f418e999a54d3423ed3e5c02f
Jason Breedlove <j@jasonbreedlove.com>
2022-05-01 13:41:05 -0700

    operator button integration

--- calcApp.js
+++ calcApp.js
@@ -1,60 +1,61 @@
-const numberElement = document.querySelectorAll('.number');
-const operatorElement = document.querySelectorAll('.operator');
-const equalElement = document.querySelector('.equal');
-const clearElement = document.querySelector('.allClear');
-const clearLastElement = document.querySelector('.clearLast');
-const decimalElement = document.querySelector('.decimal');
+const numberElement = document.querySelectorAll(".number");
+const operatorElement = document.querySelectorAll(".operator");
+const equalElement = document.querySelector(".equal");
+const clearElement = document.querySelector(".allClear");
+const clearLastElement = document.querySelector(".clearLast");
+const decimalElement = document.querySelector(".decimal");
 
-const displayElement1 = document.querySelector('.display1');
-const displayElement2 = document.querySelector('.display2');
-const tempResultElement = document.querySelector('.tempResult');
+const displayElement1 = document.querySelector(".display1");
+const displayElement2 = document.querySelector(".display2");
+const tempResultElement = document.querySelector(".tempResult");
 
 let result = null;
-let display1Num = '';
-let display2Num = '';
+let display1Num = "";
+let display2Num = "";
 let hasDecimal = false;
-let previousOperator = '';
+let previousOperator = "";
 
-numberElement.forEach(number => {
-    number.addEventListener('click', (e) => {
-        if (e.target.innerText === '.' && !hasDecimal) {
-            hasDecimal = true;
-        } else if (e.target.innerText === '.' && hasDecimal) {
-            return;
-        }
-        display2Num += e.target.innerText;
-        displayElement2.innerText = display2Num;
-
-    });
-
+numberElement.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !hasDecimal) {
+      hasDecimal = true;
+    } else if (e.target.innerText === "." && hasDecimal) {
+      return;
+    }
+    display2Num += e.target.innerText;
+    displayElement2.innerText = display2Num;
+  });
 });
 
-operatorElement.forEach(operator => {
-    operator.addEventListener('click', (e) => {
-        displayElement1.innerText = display2Num;
-    });
+operatorElement.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    if (!display2Num) {
+      hasDecimal = false;
+    }
+    const operationName = e.target.innerText;
+    if (display2Num && display1Num && lastOperation) {
+      mathOperation();
+    }
+  });
 });
 
-
-
-function mathOperation(display1Num, display2Num, operator) {
-    if (operatorElement.innerText === '+') {
-        result += parseFloat(display1Num) + parseFloat(display2Num);
-        console.log('working');
-    }
-    if (operatorElement.innerText === '-') {
-        result += parseFloat(display2Num);
-    }
-    if (operatorElement.innerText === '*') {
-        result += parseFloat(display2Num);
-    }
-    if (operatorElement.innerText === '/') {
-        result += parseFloat(display2Num);
-    }
-    if (operatorElement.innerText === '%') {
-        result += parseFloat(display2Num);
-    }
-
+function mathOperation() {
+  if (lastOperation === "+") {
+    result = parseFloat(result) + parseFloat(display2Num);
+    console.log("add working", result);
+  } else if (lastOperation === "-") {
+    result = parseFloat(result) - parseFloat(display2Num);
+    console.log("subtract working", result);
+  } else if (lastOperation === "*") {
+    result = parseFloat(result) * parseFloat(display2Num);
+    console.log("multiply working", result);
+  } else if (lastOperation === "/") {
+    result = parseFloat(result) / parseFloat(display2Num);
+    console.log("division working", result);
+  } else if (lastOperation === "%") {
+    result = parseFloat(result) % parseFloat(display2Num);
+    console.log("modulus working", result);
+  }
 }
 
 // transfer display2Num to display1Num
